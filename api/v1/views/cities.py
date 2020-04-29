2d4b3851051a04b2d1c591f272735617bf737767
Cristian <1154@holbertonschool.com>
2020-04-29 13:12:48 +0000

    Updating task 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -11,16 +11,16 @@ from models.city import City
                  strict_slashes=False)
 def Retrieve_Cities(state_id):
     """ list cities"""
-    MyState = storage.all('City')
+    MyState = storage.all()
+    obj = 'State' + '.' + state_id
     MyCity = []
-    flag = 0
-    for v in MyState.values():
-        if state_id in v.state_id:
-            MyCity.append(v.to_dict())
-            flag = 1
-    if flag == 0:
+    if obj in MyState.keys():
+        table_cities = MyState[obj].cities
+        for city in table_cities:
+                MyCity.append(city.to_dict())
+        return jsonify(MyCity)
+    else:
         return (abort(404))
-    return jsonify(MyCity)
 
 
 @app_views.route('/cities/<city_id>', methods=['GET'], strict_slashes=False)
@@ -29,9 +29,9 @@ def Retrieve_Cities2(city_id):
     MyCity = storage.all('City')
     obj = 'City' + '.' + city_id
     """ if obj not in MyCity.keys(): """
-    for k, v in MyCity.items():
-        if obj in k:
-            return jsonify(v.to_dict())
+
+    if obj in MyCity.keys():
+        return jsonify(MyCity[obj].to_dict())
     return (abort(404))
 
 
@@ -53,6 +53,12 @@ def Delete_obj(city_id):
                  strict_slashes=False)
 def Create_obj(state_id):
     """ Create Obj """
+    Id_state = storage.all('State')
+    obj = 'State' + '.' + state_id
+
+    if obj not in Id_state.keys():
+        return abort(404)
+
     try:
         data = request.get_json()
     except:
@@ -64,22 +70,22 @@ def Create_obj(state_id):
     if 'name' not in data:
         return (jsonify({'error': 'Missing name'}), 400)
 
-    Id_state = storage.all('State')
-    obj = 'State' + '.' + state_id
-
-    if obj in Id_state:
-        MyCity = City(**data)
-        MyCity.state_id = state_id
-        storage.new(MyCity)
-        storage.save()
-        return (jsonify(MyCity.to_dict()), 201)
-    else:
-        return abort(404)
+    data['state_id'] = state_id
+    MyCity = City(**data)
+    storage.new(MyCity)
+    storage.save()
+    return (jsonify(MyCity.to_dict()), 201)
 
 
 @app_views.route('/cities/<city_id>', methods=['PUT'], strict_slashes=False)
 def Update_obj(city_id):
     """ Update obj """
+    MyVar = storage.all('City')
+    obj = 'City' + '.' + city_id
+
+    if obj not in MyVar.keys():
+        return abort(404)
+
     try:
         data = request.get_json()
     except:
@@ -87,14 +93,8 @@ def Update_obj(city_id):
     if data is None:
         return (jsonify({'error': 'Not a JSON'}), 400)
 
-    MyVar = storage.all('City')
-    obj = 'City' + '.' + city_id
-    if obj in MyVar:
-        for k, v in data.items():
-            if k != 'id' or k != 'state_id' or k != 'cre\
-                                ated_at' or k != 'updated_at':
-                setattr(MyVar[obj], k, v)
+    for k, v in data.items():
+        if k != 'id' or k != 'created_at' or k != 'updated_at':
+            setattr(MyVar[obj], k, v)
         storage.save()
         return (jsonify(MyVar[obj].to_dict()), 200)
-    else:
-        return abort(404)
